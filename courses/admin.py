962844be3055944be63f350cfd26689045a6b8ac
leenadihari <leenadihari@gmail.com>
2026-03-31 18:32:27 +0530

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission
+from .models import Course, Lesson, Question, Choice, Submission, Instructor, Learner
 
 # Inline models for admin
 class ChoiceInline(admin.TabularInline):
@@ -22,8 +22,18 @@ class CourseAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
     list_display = ('name',)
 
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+class LearnerAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    filter_horizontal = ('enrolled_courses',)  # If using ManyToManyField
+
 # Register models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)  # Optional, but ensures Choices are visible
 admin.site.register(Submission)
+admin.site.register(Instructor, InstructorAdmin)
+admin.site.register(Learner, LearnerAdmin)
